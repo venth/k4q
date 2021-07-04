e4be19a062cf5c2b917f6868dac82da61604e430
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2021-07-04 14:17:06 +0200

    some exploratory tests to learn how clap works

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,27 @@ mod cli;
 
 
 fn main() {
-    cli::app();
-    println!("Hello, world!");
+    let matches = cli::app();
+
+    let query_topics = matches
+        .subcommand_matches("query")
+        .and_then(|m| m.values_of("topics"))
+        .map(|v| v.collect())
+        .unwrap_or(vec!["No topics"])
+        .join(", ");
+
+    let query_key_criteria = matches
+        .subcommand_matches("query")
+        .and_then(|m| m.subcommand_matches("key"))
+        .and_then(|m| m.value_of("criteria"))
+        .unwrap_or("no criteria");
+
+    let query_key_value = matches
+        .subcommand_matches("query")
+        .and_then(|m| m.subcommand_matches("key"))
+        .and_then(|m| m.value_of("keyValue"))
+        .unwrap_or("no value");
+
+    println!("topics: {}, criteria: {}, key value: {}",
+             query_topics, query_key_criteria, query_key_value)
 }
