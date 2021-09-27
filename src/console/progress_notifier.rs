570f0a2f9fbef0e2fb99cf39d2d97b6d3faf5a5b
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2021-09-27 19:35:46 +0200

    kaf-2 example progress bar with output

--- src/console/progress_notifier.rs
+++ src/console/progress_notifier.rs
@@ -1,10 +1,28 @@
+use std::thread;
+use std::time::Duration;
+
+use indicatif::{MultiProgress, ProgressBar, ProgressDrawTarget, ProgressStyle};
 use shaku;
 
 use crate::domain::ports;
 
 impl ports::ProgressNotifier for ConsoleErrorNotifier {
     fn notify(&self, message: &str) {
-        todo!()
+        let m = MultiProgress::new();
+        let sty = ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+            .progress_chars("##-");
+        let pb = m.add(ProgressBar::new(128));
+        pb.set_style(sty);
+        pb.set_position(0);
+        for i in 0..128 {
+            pb.set_message(format!("item #{}", i + 1));
+            pb.println(message);
+            pb.inc(1);
+            thread::sleep(Duration::from_millis(15));
+        }
+        pb.println(message);
+        pb.finish();
     }
 }
 
